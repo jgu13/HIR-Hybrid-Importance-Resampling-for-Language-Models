7c4e643d8c64455667a7f752a4b0c313028481a1
Jia Yao GU <jgu13@mcb-gpu1.CS.McGill.CA>
2024-12-07 18:58:09 -0500

    update test_hybrid_importance_estimator.py

--- test_hybrid_importance_estimator.py
+++ test_hybrid_importance_estimator.py
@@ -11,8 +11,9 @@ raw_datasets = [os.path.join(os.path.join(data_path, "toy_data.jsonl"))]
 target_datasets = [os.path.join(os.path.join(data_path, "toy_data_target.jsonl"))]
 
 dsir = HashedNgramDSIR(raw_datasets, target_datasets, cache_dir=os.path.join(PROJ_HOME, 'dsir_cache'), num_proc=1)
-# dsir.fit_ng_importance_estimator(num_tokens_to_fit='auto')
-dsir.fit_gmm_importance_estimator()
-                                # raw_text_emb_path = dsir.cache_dir / "raw text embeddings",
-                                # target_text_emb_path = dsir.cache_dir / "target text embeddings")
-dsir.compute_hybrid_importance_weight(alpha = 0.5, save_path = dsir.cache_dir / "hybrid_importance_weights_alpha_0.5.npy") # ng
+dsir.fit_ng_importance_estimator(num_tokens_to_fit='auto')
+dsir.fit_gmm_importance_estimator()    
+# uncomment to run fit_gmm_importance_estimator with pre-computed raw and text embeddings      
+# dsir.fit_gmm_importance_estimator(raw_text_emb_path = dsir.cache_dir / "raw text embeddings",
+#                                 target_text_emb_path = dsir.cache_dir / "target text embeddings")
+dsir.compute_hybrid_importance_weight(alpha = 0.5, save_path = dsir.cache_dir / "hybrid_importance_weights_alpha_0.5.npy") # TODO: try alpha = [0.0, 0.25, 0.5, 0.75, 1.0]
